f674fc94fcfce967931d9125b51570f5c00f111d
cordoba1991 <149287487+cordoba1991@users.noreply.github.com>
2024-07-16 22:18:57 -0500

    Create Topic.java

---
+++ Topic.java
@@ -0,0 +1,22 @@
+package com.example.foroHub.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    // Getters and Setters
+}
